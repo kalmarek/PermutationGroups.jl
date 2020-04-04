502abb590f753c404ddb0aa685a97570ecfc6f01
kalmarek <kalmar@amu.edu.pl>
2020-04-04 19:46:59 +0200

    update examples

--- examples/characters.jl
+++ examples/characters.jl
@@ -10,23 +10,36 @@ chars = let G = SymmetricGroup(4)
     S = gens(G)
     ccG = conjugacy_classes(G)
 
+    ccG = [
+        Orbit(S, one(G)),
+        Orbit(S, perm"(1,2)(3,4)"),
+        Orbit(S, perm"(1,2)(4)"),
+        Orbit(S, perm"(1,2,3)(4)"),
+        Orbit(S, perm"(1,2,3,4)"),
+    ]
+
+    @assert sum(length, ccG) == order(G)
+
     # Multiplication tables for conjugacy classes.
-    Ns = [PermutationGroups.CCMatrix(ccG, i) for i in 1:length(ccG)]
-    # @show Ns
+    Ns = (PermutationGroups.CCMatrix(ccG, i) for i in 1:length(ccG))
 
     F = GF(PermutationGroups.dixon_prime(ccG))
-
-    basis = PermutationGroups.sd_basis(Ns, F)
-    ib, b = inv(basis.basis), basis.basis
+    # F = GF(41)
+    eig_decomposition = PermutationGroups.sd_basis(Ns, F)
+    display(eig_decomposition)
     for N in Ns
         # basis actually diagonalizes all Ns
-        @test isdiag(Matrix(ib*matrix(F,N)*b))
+        m = PermutationGroups._change_basis(matrix(F,N), eig_decomposition.basis)
+        display(m)
+        @test isdiag(Matrix(m))
     end
-    display(basis)
-    chars = [PermutationGroups.Character(vec(v), ccG) for v in basis]
+
+    chars = [PermutationGroups.Character(
+        vec(eig_space), ccG) for eig_space in eig_decomposition]
 
     # orthogonality of characters:
-    @test isdiag([dot(χ, ψ) for χ in chars, ψ in chars])
+    @test [dot(χ, ψ) for χ in chars, ψ in chars] ==
+        Matrix{Int}(I, length(chars), length(chars))
 
     chars
 end
@@ -37,10 +50,14 @@ hcat([χ.vals for χ in chars])
 chars = let G = PermGroup([perm"(1,2,4,5,3)", perm"(2,5,3,4)"]);
     @test order(G) == 20
 
+    ccG = conjugacy_classes(G)
+    @test sum(length, ccG) == 20
+
     S = gens(G)
     a,b = S
 
-    ccG = [Orbit(S, one(G)),
+    ccG = [
+        Orbit(S, one(G)),
         Orbit(S, a),
         Orbit(S, b),
         Orbit(S, b^2),
@@ -52,20 +69,23 @@ chars = let G = PermGroup([perm"(1,2,4,5,3)", perm"(2,5,3,4)"]);
 
     F = GF(PermutationGroups.dixon_prime(ccG))
     # F = GF(41)
-    basis = PermutationGroups.sd_basis(Ns, F)
-    display(basis)
-    ib, b = inv(basis.basis), basis.basis
+    eig_decomposition = PermutationGroups.sd_basis(Ns, F)
+    display(eig_decomposition)
     for N in Ns
         # basis actually diagonalizes all Ns
-        @test isdiag(Matrix(ib*matrix(F,N)*b))
+        m = PermutationGroups._change_basis(matrix(F,N), eig_decomposition.basis)
+        display(m)
+        @test isdiag(Matrix(m))
     end
 
-    χ = [PermutationGroups.Character(vec(v), ccG) for v in basis]
+    chars = [PermutationGroups.Character(
+        vec(eig_space), ccG) for eig_space in eig_decomposition]
 
     # orthogonality of characters:
-    @test isdiag([dot(χ[i], χ[j]) for i in 1:length(χ), j in 1:length(χ)])
+    @test [dot(χ, ψ) for χ in chars, ψ in chars] ==
+        Matrix{Int}(I, length(chars), length(chars))
 
-    χ
+    chars
 end
 
 hcat([χ.vals for χ in chars])
