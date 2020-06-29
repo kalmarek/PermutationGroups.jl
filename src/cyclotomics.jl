31000a6096795e49d9afeb8781fd642a085ad2f4
kalmarek <kalmar@amu.edu.pl>
2020-06-29 18:10:07 +0200

    first shot at embed function

--- src/cyclotomics.jl
+++ src/cyclotomics.jl
@@ -259,11 +259,18 @@ function Base.hash(α::Cyclotomic, h::UInt)
     return hash(coeffs(α), hash(conductor(α), hash(Cyclotomic, h)))
 end
 
-function Base.:(==)(α::Cyclotomic, β::Cyclotomic)
-    conductor(α) == conductor(β) || throw("Embeddings are not implemented yet")
+Base.:(==)(α::Cyclotomic, β::Cyclotomic) =
+    ==(α, β, Val{conductor(α)==conductor(β)}())
+
+function Base.:(==)(α::Cyclotomic, β::Cyclotomic, ::Val{true})
     normalform!(α)
     normalform!(β)
     return coeffs(α) == coeffs(β)
+end
+
+function Base.:(==)(α::Cyclotomic, β::Cyclotomic, ::Val{false})
+    l = lcm(conductor(α), conductor(β))
+    return ==(embed(α, l), embed(β, l), Val{true}())
 end
 
 Base.iszero(α::Cyclotomic) = all(iszero, values(α)) || (normalform!(α); all(iszero, values(α)))
@@ -276,6 +283,7 @@ normalform(α::Cyclotomic) = normalform!(deepcopy(α))
 
 function normalform!(α::Cyclotomic{T}, tmp=Cyclotomic{T, Vector{T}}(conductor(α), coeffs(α))) where T
     n = conductor(α)
+    @debug "normalizing cyclotomic" conductor(α) coeffs(α)
 
     basis, forbidden = let (b, fb) =_zumbroich_complement(n)
         BitSet(b), fb
@@ -302,6 +310,11 @@ function _replace_exponent!(α::Cyclotomic, exp::Integer, fb)
     if exp % q ∈ forbidden_exps
         # @debug "removing $(α[exp])*ζ^$exp from" α
         m = conductor(α) ÷ p
+        @debug " " collect(α[exp .+ 1:m:m*(p-1)])
+
+        # TODO: this doesn't work
+        # α[exp .+ 1:m:m*(p-1)] .-= val
+
         for i in 1:p-1
             α[exp + i*m] -= val
             # exp + i*m is not guaranteed to be allowed, but it's larger than exp and hence will be dealt later
@@ -310,6 +323,121 @@ function _replace_exponent!(α::Cyclotomic, exp::Integer, fb)
         # @debug "after removal:" α
     end
     return α
+end
+
+function embed(α::Cyclotomic, m::Integer)
+    conductor(α) == m && return copy(α)
+    if conductor(α) > m
+        return reduced_embedding(α, m)
+    else
+        k, _r = divrem(m, conductor(α))
+        @assert _r == 0 "Embeding of ℚ(ζ$(subscriptify(conductor(α)))) ↪ ℚ(ζ$(subscriptify(m))) is not possible."
+
+        res = zero!(similar(α, valtype(α), m))
+
+        @inbounds for e in exponents(α)
+            res[e*k] = α[e]
+        end
+        return res
+    end
+end
+
+_all_equal(α::Cyclotomic, exps, value) = all(==(value), (α[e] for e in exps))
+
+function reduced_embedding(α::Cyclotomic{T,V}, m::Integer=1) where {T, V}
+    @assert conductor(α) % m == 0 "The embedding field must be a subfiled of α"
+
+    k = gcd(exponents(α)...)
+    @debug "gcd(exponents(α)) = $k" collect(exponents(α))
+
+    tmp = let (d,r) = divrem(conductor(α), k)
+        if (k > 1 && iszero(r))
+            @debug "Reducing the embedding ring to $T(ζ$(subscriptify(d)))"
+            tmp = Cyclotomic{T, Vector{T}}(d, zeros(T, d))
+        else
+            n = conductor(α)
+            tmp = Cyclotomic{T, Vector{T}}(n, zeros(T, n))
+        end
+        tmp # tmp has dense storage
+    end
+
+    if k > 1
+        # the trivial reduction E(n)^e → E(n÷k)^(e÷k)
+        @debug "Performing trivial reduction from ℚ(ζ$(subscriptify(n))) → ℚ(ζ$(subscriptify(n÷k)))"
+        for (e,v) in zip(exponents(α), values(α))
+            tmp[div(e,k)] = α[e]
+        end
+    else
+        @debug "No trivial reduction is possible"
+        tmp.coeffs .= coeffs(α)
+    end
+
+    normalform!(tmp)
+
+    basis, forbidden = Cyclotomics._zumbroich_complement(conductor(tmp))
+
+    phi_nc = length(basis)
+    nz = count(!iszero, coeffs(tmp))
+    val = first(values(tmp))
+
+    if all(p == q for (p,q,_) in forbidden) && # conductor(tmp) is square-free
+        phi_nc == nz && # tmp is supported on φ(n) elements
+        _all_equal(tmp, exponents(tmp), val) # all nz coeffs of tmp are equal
+
+        @debug "Cyclotomic is real:" α = -val
+        res = Cyclotomic{T, V}(m, similar(coeffs(α), m))
+        zero!(res)
+        res[0] = -val
+        return res
+    end
+
+    n = conductor(tmp)
+    for (p,q,fb_res) in forbidden
+
+        p != q && continue
+        @debug "$(p)² does NOT divide conductor $(conductor(tmp))" p q
+
+        m % p == 0 && continue
+        @debug "prime $p does not divide $m so embedding is possible"
+
+        # there are nc/p residue classes, each contains p-1 elts, and tmp is constant on each
+        nnz, _r = divrem(nz, p-1)
+        @debug "the number of non-zero coeffs $nz ≡ $_r mod $(p-1)"
+        _r != 0 && continue
+
+
+        n╱p = n ÷ p
+
+        # check that coeffs(tmp) are constant on each congruence classes
+        equal_on_classes = true
+        for i in 1:p:n
+            if !_all_equal(tmp, i.+(2n╱p:n╱p:n+i), tmp[i + n╱p])
+                equal_on_classes = false
+                @debug "skipping reduction over prime $p as cyclotomic is not constant over class $(i+(n╱p)) (mod $(n╱p)) " tmp[i.+(2n╱p:n╱p:n+i)]
+                break
+            end
+        end
+        equal_on_classes || continue # to next prime
+
+        @debug "cyclotomic is constant over residue classes (mod $(n╱p)), reducing"
+
+
+        for i in 1:p:n # inverse to normalform over p
+            tmp[i] = -tmp[i+n╱p]
+            tmp[(i+n╱p):n╱p:(n╱p+i)] = zero(T)
+        end
+
+        for i in 1:n╱p
+            tmp[i] = tmp[i*p]
+            tmp[i*p] = zero(T)
+        end
+
+        nz = nnz # update the number on non-zero coefficients
+        tmp = Cyclotomic{T, Vector{T}}(np, resize!(coeffs(tmp), np))
+    end
+
+
+    return Cyclotomic{T, V}(conductor(tmp), coeffs(tmp))
 end
 
 ####
