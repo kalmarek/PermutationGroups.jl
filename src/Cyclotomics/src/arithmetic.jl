37483e7b1c4766739128319b907be7fe5e9ffac6
kalmarek <kalmar@amu.edu.pl>
2020-07-16 12:51:47 +0200

    reorganize arithmetic.jl

--- src/Cyclotomics/src/arithmetic.jl
+++ src/Cyclotomics/src/arithmetic.jl
@@ -6,30 +6,16 @@ Base.zero(α::Cyclotomic, m::Integer=conductor(α)) =
     (res = similar(α, m); zero!(res))
 Base.one(α::Cyclotomic{T}) where T = (res = zero(α); res[0] = one(T); res)
 
-add!(out::Cyclotomic, α::Cyclotomic, β::Cyclotomic) =
-    (coeffs(out) .= coeffs(α) .+ coeffs(β); out)
-sub!(out::Cyclotomic, α::Cyclotomic, β::Cyclotomic) =
-    (coeffs(out) .= coeffs(α) .- coeffs(β); out)
-mul!(out::Cyclotomic, α::Cyclotomic, c::Real) =
-    (coeffs(out) .= coeffs(α) .* c; out)
-div!(out::Cyclotomic, α::Cyclotomic, c::Real) =
-    (coeffs(out) .= div.(coeffs(α), c); out)
+############################
+# Module structure:
 
-function Base.:+(α::Cyclotomic{T}, β::Cyclotomic{S}) where {T,S}
-    conductor(α) == conductor(β) || throw("Not Implemented")
-    return add!(similar(α, promote_type(T, S)), α, β)
-end
-
-function Base.:-(α::Cyclotomic{T}, β::Cyclotomic{S}) where {T,S}
-    conductor(α) == conductor(β) || throw("Not Implemented")
-    return sub!(similar(α, promote_type(T, S)), α, β)
-end
+Base.:-(α::Cyclotomic) = Cyclotomic(-coeffs(α))
 
 for op in (:+, :-)
     @eval begin
         function Base.$op(α::Cyclotomic{T}, r::R) where {T, R<:Real}
             res = similar(α, promote_type(T, R))
-            coeffs(res) .= coeffs(α)
+            copyto!(coeffs(res), coeffs(α))
             res[0] = $op(res[0], r)
             return res
         end
@@ -38,32 +24,69 @@ end
 
 Base.:+(r::Real, α::Cyclotomic) = α + r
 Base.:-(r::Real, α::Cyclotomic) = (res = -α; res[0] += r; res)
-Base.:-(α::Cyclotomic) = Cyclotomic(-coeffs(α))
-Base.:*(c::T, α::Cyclotomic{S}) where {S,T<:Number} =
+
+mul!(out::Cyclotomic, α::Cyclotomic, c::Real) =
+    (coeffs(out) .= coeffs(α) .* c; out)
+div!(out::Cyclotomic, α::Cyclotomic, c::Real) =
+    (coeffs(out) .= div.(coeffs(α), c); out)
+
+Base.:*(c::T, α::Cyclotomic{S}) where {S,T<:Real} =
     mul!(similar(α, promote_type(S,T)), α, c)
-Base.:*(α::Cyclotomic, c::T) where T<:Number = c*α
-Base.:(//)(α::Cyclotomic, c::Number) = Cyclotomic(coeffs(α).//c)
-Base.:(/)(α::Cyclotomic, c::Number) = Cyclotomic(coeffs(α)./c)
+Base.:*(α::Cyclotomic, c::T) where T<:Real = c*α
+Base.:(//)(α::Cyclotomic, c::Real) = Cyclotomic(coeffs(α).//c)
+Base.:(/)(α::Cyclotomic, c::Real) = Cyclotomic(coeffs(α)./c)
 
 function Base.div(α::Cyclotomic, c::Number)
     res = similar(α, first(Base.return_types(div, (valtype(α), typeof(c)))))
     return div!(res, α, c)
 end
 
-function mul!(out::Cyclotomic{T}, α::Cyclotomic, β::Cyclotomic) where T
-    if out === α || out === β
-        out = deepcopy(out)
-    end
-    tmp = Cyclotomic{T, Vector{T}}(conductor(out), zeros(T, conductor(out)))
+###########################
+# Ring structure:
 
-    for (αe, αc) in zip(exponents(α), values(α))
-        for (βe, βc) in zip(exponents(β), values(β))
-            tmp[αe + βe] += αc*βc
-        end
-    end
-    zero!(out)
-    out.coeffs .= coeffs(tmp)
+add!(out::Cyclotomic, α::Cyclotomic, β::Cyclotomic) =
+    (coeffs(out) .= coeffs(α) .+ coeffs(β); out)
+sub!(out::Cyclotomic, α::Cyclotomic, β::Cyclotomic) =
+    (coeffs(out) .= coeffs(α) .- coeffs(β); out)
+
+function mul!(out::Cyclotomic{T}, α::Cyclotomic, β::Cyclotomic) where T
+    tmp = Cyclotomic{T, Vector{T}}(conductor(out), zeros(T, conductor(out)))
+    mul!(tmp, α, β)
+    copyto!(coeffs(out), coeffs(tmp))
     return out
 end
 
-Base.:*(α::Cyclotomic, β::Cyclotomic) = mul!(similar(α), α, β)
+function mul!(
+    out::Cyclotomic{T,<:DenseVector},
+    α::Cyclotomic,
+    β::Cyclotomic,
+) where {T}
+    if out === α || out === β
+        out = similar(out)
+    end
+    zero!(out)
+
+    for (αe, αc) in zip(exponents(α), values(α))
+        for (βe, βc) in zip(exponents(β), values(β))
+            out[αe+βe] += αc * βc
+        end
+    end
+
+    return out
+end
+
+for (op, fn) in ((:+, :add!), (:-, :sub!), (:*, :mul!))
+    @eval begin
+        Base.$op(α::Cyclotomic, β::Cyclotomic) =
+            $op(α, β, Val{conductor(α)==conductor(β)}())
+
+        function Base.$op(α::Cyclotomic{T}, β::Cyclotomic{S}, ::Val{true}) where {T,S}
+            return $fn(similar(α, promote_type(T, S)), α, β)
+        end
+
+        function Base.$op(α::Cyclotomic, β::Cyclotomic, ::Val{false})
+            l = lcm(conductor(α), conductor(β))
+            return $op(embed(α, l), embed(β, l), Val{true}())
+        end
+    end
+end
