37d8b75f0e0808985eecad372434c0926d08e5ec
kalmarek <kalmar@amu.edu.pl>
2020-07-16 11:52:24 +0200

    working reduced_embedding!

--- src/Cyclotomics/src/normalform.jl
+++ src/Cyclotomics/src/normalform.jl
@@ -76,6 +76,9 @@ function embed(α::Cyclotomic, m::Integer)
 end
 
 function _tmp_for_reduced_embedding(α::Cyclotomic{T}) where T
+
+    all(iszero, exponents(α)) && return Cyclotomic{T, Vector{T}}(1, [α[0]])
+
     k = gcd(exponents(α)...)
     @debug "gcd(exponents(α)) = $k" collect(exponents(α))
 
@@ -112,12 +115,12 @@ function reduced_embedding(α::Cyclotomic{T,V}, m::Integer=1;
 
     phi_nc = length(basis)
     nz = count(!iszero, coeffs(tmp))
-    val = first(values(tmp))
 
     if all(p == q for (p,q,_) in forbidden) && # conductor(tmp) is square-free
         phi_nc == nz && # tmp is supported on φ(n) elements
-        _all_equal(tmp, exponents(tmp), val) # all nz coeffs of tmp are equal
+        _all_equal(tmp, exponents(tmp)) # all nz coeffs of tmp are equal
 
+        val = tmp[first(exponents(tmp))]
         @debug "Cyclotomic is real:" α = -val
         res = Cyclotomic{T, V}(m, similar(coeffs(α), m))
         zero!(res)
@@ -125,51 +128,69 @@ function reduced_embedding(α::Cyclotomic{T,V}, m::Integer=1;
         return res
     end
 
-    n = conductor(tmp)
     for (p,q,fb_res) in forbidden
+        n = conductor(tmp)
+        nz = count(!iszero, coeffs(tmp))
 
-        p != q && continue
+        p == q || continue
         @debug "$(p)² does NOT divide conductor $(conductor(tmp))" p q
 
-        m % p == 0 && continue
+        m % p != 0 || continue
         @debug "prime $p does not divide $m so embedding is possible"
 
-        # there are nc/p residue classes, each contains p-1 elts, and tmp is constant on each
+        # there are n/p residue classes, each contains p-1 elts, and tmp is constant on each
         nnz, _r = divrem(nz, p-1)
         @debug "the number of non-zero coeffs $nz ≡ $_r mod $(p-1)"
-        _r != 0 && continue
+        _r == 0 || continue
 
+        # Check that coeffs(tmp) are constant on each congruence classes.
+        # To illustrate by example write:
+        #
+        # 1 = - ζ₅ - ζ₅² - ζ₅³ - ζ₅⁴
+        #
+        # and consider
+        #
+        # c*ζ₉^e = c*ζ₉^e * 1 = c*ζ₉^e * (- ζ₅ - ζ₅² - ζ₅³ - ζ₅⁴) =
+        # - c*ζ₄₅^(5e+1*9) - c*ζ₄₅^(5e+2*9) - c*ζ₄₅^(5e+3*9) - c*ζ₄₅^(5e+4*9)
+        #
+        # If tmp ∈ ℚ(ζ_n) can be reduced to ℚ(ζ_{n÷p})
+        # (n = 45, p = 5 in the example), we need to check that all coefficients
+        #
+        # tmp[p*e + i*n÷p] are equal (for 1 ≤ i ≤ p-1)
+        #
+        # where exponents 0 ≤ e ≤ n-1 represent congruence classes modulo n÷p
 
-        n╱p = n ÷ p
+        n_p = n ÷ p
 
-        # check that coeffs(tmp) are constant on each congruence classes
         equal_on_classes = true
-        for i in 1:p:n
-            if !_all_equal(tmp, i.+(2n╱p:n╱p:n+i), tmp[i + n╱p])
+        for pe in 0:p:(n-1)
+            equal_on_classes || break
+            if !_all_equal(tmp, pe.+(n_p.*(1:p-1)))
                 equal_on_classes = false
-                @debug "skipping reduction over prime $p as cyclotomic is not constant over class $(i+(n╱p)) (mod $(n╱p)) " tmp[i.+(2n╱p:n╱p:n+i)]
-                break
+                @debug "skipping reduction over prime $p as cyclotomic is not constant over class $(pe + (n_p)) (mod $(n_p)) " (pe + n_p, tmp[pe + n_p]) (pe.+(n_p.*(2:p-1)), tmp[pe.+(n_p.*(2:p-1))])
             end
         end
-        equal_on_classes || continue # to next prime
+        equal_on_classes || continue # to the next prime
 
-        @debug "cyclotomic is constant over residue classes (mod $(n╱p)), reducing"
+        @debug "cyclotomic is constant over residue classes (mod $(n_p)), reducing"
 
-
-        for i in 1:p:n # inverse to normalform over p
-            tmp[i] = -tmp[i+n╱p]
-            tmp[(i+n╱p):n╱p:(n╱p+i)] = zero(T)
+        for pe in 0:p:(n-1)
+            # replace sum_i(tmp[p*e + i*n÷p]) (1 ≤ i ≤ p-1)
+            # inverse to normalform! over p
+            @debug "replacing $pe-th power by $(pe + n_p)-th:" tmp[pe] -tmp[pe + n_p]
+            tmp[pe] = -tmp[pe + n_p] # tmp[pe + 0*n_p] = -tmp[pe + 1*n_p]
+            tmp[pe.+(n_p.*(1:p-1))] .= zero(T)
         end
 
-        for i in 1:n╱p
+        for i in 1:n_p
             tmp[i] = tmp[i*p]
             tmp[i*p] = zero(T)
         end
 
-        nz = nnz # update the number on non-zero coefficients
-        tmp = Cyclotomic{T, Vector{T}}(np, resize!(coeffs(tmp), np))
+        tmp = Cyclotomic{T, Vector{T}}(n_p, resize!(tmp.coeffs, n_p))
     end
 
+    res = Cyclotomic{T, V}(conductor(tmp), coeffs(tmp))
 
-    return Cyclotomic{T, V}(conductor(tmp), coeffs(tmp))
+    return res
 end
